0985a673b07337327d0484cac4dc18f09d8b6945
yanghuadr <yanghuadr@gmail.com>
2025-12-30 20:14:07 +0000

    Deploying to gh-pages from @ yanghuadr/yanghuadr.github.io@8905f02d928a3a9fd42dcad7f2f43a70238d054f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,20 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-awards",
+          title: "Awards",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/awards/";
+          },
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
